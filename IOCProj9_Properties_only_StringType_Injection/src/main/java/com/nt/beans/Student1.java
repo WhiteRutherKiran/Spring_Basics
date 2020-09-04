44ec01333cda37e150f9e0796f02e5892f70ec5f
user <user@user-PC>
2020-09-04 16:47:14 +0530

    only String & primitive not support object type

---
+++ IOCProj9_Properties_only_StringType_Injection/src/main/java/com/nt/beans/Student1.java
@@ -0,0 +1,23 @@
+package com.nt.beans;
+import java.util.Map;
+import java.util.Properties;
+
+public class Student1 {
+	private Properties propes;
+
+	public Properties getPropes() {
+		return propes;
+	}
+
+	public void setPropes(Properties propes) {
+		this.propes = propes;
+	}
+
+	@Override
+	public String toString() {
+		return "Student1 [propes=" + propes + "]";
+	}
+	
+	
+
+}
